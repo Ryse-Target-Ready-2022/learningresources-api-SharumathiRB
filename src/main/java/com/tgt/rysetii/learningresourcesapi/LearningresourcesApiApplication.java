71adcf67f935de89b3780bec55f4fce55bdd9288
Sharumathi1818 <80836126+Sharumathi1818@users.noreply.github.com>
2022-07-11 21:54:10 +0530

    Methods-Collections done

--- src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
+++ src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
@@ -1,5 +1,6 @@
 package com.tgt.rysetii.learningresourcesapi;
 
+import com.tgt.rysetii.learningresourcesapi.service.LearningResourceService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
