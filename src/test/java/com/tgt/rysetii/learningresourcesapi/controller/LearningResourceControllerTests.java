e65141aeb8e2f563211a82888ac3e64712d0738e
Sharumathi1818 <80836126+Sharumathi1818@users.noreply.github.com>
2022-07-22 21:27:47 +0530

    Controller modified

---
+++ src/test/java/com/tgt/rysetii/learningresourcesapi/controller/LearningResourceControllerTests.java
@@ -0,0 +1,9 @@
+package com.tgt.rysetii.learningresourcesapi.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class LearningResourceControllerTests {
+
+}
