98d6fa07322ccc9b9fa4aba4e8a588dbeec1f9b8
Sharumathi1818 <80836126+Sharumathi1818@users.noreply.github.com>
2022-07-22 23:35:21 +0530

    Hello World done

--- src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
+++ src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
@@ -1,23 +1,17 @@
 package com.tgt.rysetii.learningresourcesapi;
 
-import com.tgt.rysetii.learningresourcesapi.entity.LearningResource;
-import com.tgt.rysetii.learningresourcesapi.entity.LearningResourceStatus;
-import com.tgt.rysetii.learningresourcesapi.service.LearningResourceService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class LearningresourcesApiApplication {
 
 	public static void main(String[] args) {
 
-		ApplicationContext ctx= SpringApplication.run(LearningresourcesApiApplication.class, args);
+		SpringApplication.run(LearningresourcesApiApplication.class, args);
+		System.out.println("Hello World");
 
 
 	}
--- src/main/java/com/tgt/rysetii/learningresourcesapi/repository/LearningResourceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tgt.rysetii.learningresourcesapi.repository;
-
-import com.tgt.rysetii.learningresourcesapi.entity.LearningResource;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LearningResourceRepository extends JpaRepository<LearningResource,Integer> {
-}
