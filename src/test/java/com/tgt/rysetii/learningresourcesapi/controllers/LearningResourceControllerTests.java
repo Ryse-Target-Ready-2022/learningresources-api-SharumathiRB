f744bb7ebff76d69a866bc59c6490eb87e95b51a
Sharumathi1818 <80836126+Sharumathi1818@users.noreply.github.com>
2022-07-22 23:26:08 +0530

    Controller tests done

--- src/test/java/com/tgt/rysetii/learningresourcesapi/controllers/LearningResourceControllerTests.java
+++ src/test/java/com/tgt/rysetii/learningresourcesapi/controllers/LearningResourceControllerTests.java
@@ -9,14 +9,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(LearningResourceController.class)
 public class LearningResourceControllerTests {
@@ -24,35 +29,34 @@ public class LearningResourceControllerTests {
     @MockBean
     private LearningResourceService learningResourceService;
 
-    /**
-     *  Spring Boot automatically provides beans like ObjectMapper instance to map to and from JSON,
-     *  MockMvc instance to simulate HTTP requests, which we can autowire from the application context
-     * */
     @Autowired
     private ObjectMapper objectMapper;
 
     @Autowired
     private MockMvc mockMvc;
 
+    LearningResource learningResource1 = new LearningResource(1311, "Test Name 1", 100.0, 120.0, LearningResourceStatus.LIVE, LocalDate.now(), LocalDate.now().plusMonths(5), LocalDate.now().plusYears(2));
+    LearningResource learningResource2 = new LearningResource(1312, "Test Name 2", 120.0, 180.0, LearningResourceStatus.LIVE, LocalDate.now(), LocalDate.now().plusMonths(6), LocalDate.now().plusYears(3));
+
     @Test
-    public void getAllTheAvailableLearningResources() throws Exception {
-        List<LearningResource> learningResources = new ArrayList<>();
-        LearningResource learningResource1 = new LearningResource(1311, "Test Name 1", 100.0, 120.0, LearningResourceStatus.LIVE, LocalDate.now(), LocalDate.now().plusMonths(5), LocalDate.now().plusYears(2));
-        LearningResource learningResource2 = new LearningResource(1312, "Test Name 2", 120.0, 180.0, LearningResourceStatus.LIVE, LocalDate.now(), LocalDate.now().plusMonths(6), LocalDate.now().plusYears(3));
-        learningResources.add(learningResource1);
-        learningResources.add(learningResource2);
-
+    public void testGetResources() throws Exception {
+        List<LearningResource> learningResources = new ArrayList<>(Arrays.asList(learningResource1,learningResource2));
         String expectedResponse = objectMapper.writeValueAsString(learningResources);
-
         when(learningResourceService.getLearningResources()).thenReturn(learningResources);
-
-      //  MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get("/learningresources/v1/"))
-           //     .andExpect(status().isOk())
-            //    .andReturn();
-
-      //  assertEquals(expectedResponse, mvcResult.getResponse().getContentAsString());
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get("/learningresources/v1/")).andExpect(status().isOk()).andReturn();
+        assertEquals(expectedResponse, mvcResult.getResponse().getContentAsString());
     }
 
+    @Test
+    public void testSaveLearningResources() throws Exception{
+        List<LearningResource> learningResourceList=new ArrayList<>(Arrays.asList(learningResource1,learningResource2));
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/learningresources/v1/").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(learningResourceList))).andExpect(status().isCreated());
+    }
 
+    @Test
+    public void testDeleteResource() throws Exception{
+        int resourceId=1234;
+        this.mockMvc.perform(MockMvcRequestBuilders.delete("/learningresources/v1/learningresource/" + resourceId)).andExpect(status().isOk());
+    }
 
 }
